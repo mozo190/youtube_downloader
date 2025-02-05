b946a7b3d0cd035a87ea5c301fc9bc338fd99367
Molnár Zoltán <mozo37@gmail.com>
2025-02-05 18:28:07 +0100

    Implement download functionality for audio and video in YouTube Downloader

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QLineEdit, QPushButton, QLabel, QComboBox
+import os
 
+from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QLineEdit, QPushButton, QLabel, QComboBox
+from youtube3 import youtube
 
 class YouTubeDownloader(QWidget):
     def __init__(self):
@@ -10,7 +12,7 @@ class YouTubeDownloader(QWidget):
 
         self.layout = QVBoxLayout()
         self.layout.setSpacing(0)
-        self.layout.setContentsMargins(0, 0, 0, 0)
+        self.layout.setContentsMargins(20, 0, 20, 0)
 
         self.url_input = QLineEdit()
         self.url_input.setStyleSheet("background-color: white;")
@@ -27,12 +29,40 @@ class YouTubeDownloader(QWidget):
 
         self.download_btn = QPushButton("Download")
         self.download_btn.setStyleSheet("background-color: #4CAF50; color: orange; ")
+        self.download_btn.clicked.connect(self.start_download)
         self.layout.addWidget(self.download_btn)
 
         self.status_label = QLabel("Status: ")
         self.layout.addWidget(self.status_label)
 
         self.setLayout(self.layout)
+
+    def start_download(self):
+        self.status_label.setText("Status: Downloading...")
+        url = self.url_input.text()
+        format_choice = self.format_combo.currentText()
+
+        if format_choice == "audio":
+            self.download_audio(url)
+        else:
+            self.download_video(url)
+        self.status_label.setText("Status: Download completed!")
+
+    def download_audio(self, url, path="downloads"):
+        print(f"Download audio from {url}")
+        try:
+            yt = YouTube(url)
+            stream = yt.streams.filter(only_audio=True).first()
+            if not os.path.exists(path):
+                os.makedirs(path)
+            out_file = stream.download(output_path=path)
+            base, ext = os.path.splitext(out_file)
+            new_file = base + ".mp3"
+            os.rename(out_file, new_file)
+        except Exception as e:
+            print(e)
+            self.status_label.setText("Status: Download failed!", e)
+
 
 if __name__ == "__main__":
     app = QApplication([])
